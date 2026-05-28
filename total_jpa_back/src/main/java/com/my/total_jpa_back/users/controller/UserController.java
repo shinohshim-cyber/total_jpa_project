0d6b68b8d9a255b67e2d2c5c5e5b1b372fb3c393
sos-cyber <shinohshim@gmail.com>
2026-05-28 12:40:07 +0900

    feat : restful api

--- total_jpa_back/src/main/java/com/my/total_jpa_back/users/controller/UserController.java
+++ total_jpa_back/src/main/java/com/my/total_jpa_back/users/controller/UserController.java
@@ -1,22 +1,43 @@
 package com.my.total_jpa_back.users.controller;
+import com.my.total_jpa_back.common.entity.Gender;
 import com.my.total_jpa_back.users.entity.Users;
 import com.my.total_jpa_back.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 //  Restful 한 API를 제공할 때 사용하는 어노테이션
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
     private final UserRepository userRepository;
     //  전체 리스트를 요청
-    @GetMapping
+    @GetMapping("/users")
     public List<Users> findAll(){
         return userRepository.findAll();
+    }
+    @GetMapping("/gender/{gender}")
+    public List<Users> findByGender(@PathVariable Gender gender){
+        return userRepository.findByGender(gender);
+    }
+    @GetMapping("/name")
+    public List<Users> findByName(@RequestParam String keyword){
+        return userRepository.findByNameContaining(keyword);
+    }
+    @GetMapping("/color")
+    public List<Users> findByLikeColor(@RequestParam String color){
+        return userRepository.findByLikeColor(color);
+    }
+    @GetMapping("/gender-color")
+    public List<Users> findByLikeColorAndGender(@RequestParam String color,
+                                                @RequestParam Gender gender)
+    {
+        return userRepository.findByLikeColorAndGender(color, gender);
+    }
+    @GetMapping("/email")
+    public List<Users> findByEmail(@RequestParam String keyword){
+        return userRepository.findByEmailContaining(keyword);
     }
 }
