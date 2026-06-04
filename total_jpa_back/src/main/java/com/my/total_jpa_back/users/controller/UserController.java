a76e82192582c732b91491919719d32b60c1d159
sos-cyber <shinohshim@gmail.com>
2026-06-04 14:59:31 +0900

    feat : 프론트와 백앤드 연결

--- total_jpa_back/src/main/java/com/my/total_jpa_back/users/controller/UserController.java
+++ total_jpa_back/src/main/java/com/my/total_jpa_back/users/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserRepository userRepository;
     private final UserService userService;
