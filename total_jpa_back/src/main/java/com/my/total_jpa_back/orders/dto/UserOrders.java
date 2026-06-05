2d3320089b2972a3ca2c9917a7f21171c0bae378
sos-cyber <shinohshim@gmail.com>
2026-06-05 12:39:57 +0900

    feat : queryDSL 사용

---
+++ total_jpa_back/src/main/java/com/my/total_jpa_back/orders/dto/UserOrders.java
@@ -0,0 +1,17 @@
+package com.my.total_jpa_back.orders.dto;
+
+import com.my.total_jpa_back.common.entity.OrderStatus;
+import com.my.total_jpa_back.users.entity.Users;
+import lombok.Getter;
+import lombok.Setter;
+
+//  QueryDSL 검색 결과를 담을 DTO
+@Getter
+@Setter
+public class UserOrders {
+    private Long id;
+    private Users user;
+    private String productName;
+    private Integer price;
+    private OrderStatus status;
+}
---
+++ total_jpa_back/src/test/java/com/my/total_jpa_back/orders/repository/QueryDslOrderRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.my.total_jpa_back.orders.repository;
+
+import com.my.total_jpa_back.common.entity.OrderStatus;
+import com.my.total_jpa_back.orders.dto.OrderSearchCondition;
+import com.my.total_jpa_back.orders.entity.UserOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.DialectOverride;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@Slf4j
+class QueryDslOrderRepositoryTest {
+    @Autowired
+    QueryDslOrderRepository queryDslOrderRepository;
+
+    @Test
+    @DisplayName("QueryDSL로 동적 검색하기")
+    public void dslSearchTest(){
+        //  검색 조건 DTO 생성
+        OrderSearchCondition condition = new OrderSearchCondition();
+        condition.setStatus(OrderStatus.COMPLETE);
+        condition.setMinPrice(100000);
+        condition.setMaxPrice(350000);
+        condition.setUserName("kim");
+
+        List<UserOrder> result = queryDslOrderRepository.search(condition);
+        for(UserOrder userOrder : result) {
+            log.info(userOrder.toString());
+        }
+    }
+}
